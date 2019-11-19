8b7244e63b694be59cf039c452319d1e54b188e0
Ramesh Marikhu <marikhu@gmail.com>
2019-11-19 20:46:57 +0700

    Testing with a minimal gt.txt and res.txt in TEST directory

--- run.sh
+++ run.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 set -e
 
+# MOT16-11
 #python3 evaluate_tracking.py --seqmap seqmaps/test.txt --track data/ --gt data/
-python3 evaluate_tracking.py --seqmap seqmaps/test2.txt --track data/ --gt data/
+
+# TUD-Campus -- updated gt.txt to contain 1,1,1 instead of 1,-1,-1,-1 as
+# it seems mot_evaluation supports only 2D
+#python3 evaluate_tracking.py --seqmap seqmaps/test2.txt --track data/ --gt data/
+
+# TEST -- Testing minimal contents of gt.txt and res.txt
+python3 evaluate_tracking.py --seqmap seqmaps/test3.txt --track data/ --gt data/
